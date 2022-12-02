3a2635a09e0cf8fde740b52a118967d6ad86b728
機車 <119617511+ivantw829@users.noreply.github.com>
2022-12-02 20:55:53 +0800

    Pycord Bot v1.1
    Pycord Bot v1.1

---
+++ cogs/main.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+import discord
+from discord.commands import Option, slash_command
+from discord.ext import commands
+
+
+class main(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(description="查看機器人功能資訊")
+    async def help(self, ctx):
+        with open("help.txt", "r", encoding="UTF-8") as file:
+            data = file.read()
+        embed = discord.Embed(
+            title="Commands", description=data, color=discord.Colour.random()
+        )
+        await ctx.respond(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(main(bot))
---
+++ readme.md
@@ -0,0 +1,3 @@
+# py-cord 機器人模板
+
+[Discord 群](https://discord.gg/ouou) | [Youtube](https://youtube.com/@ouoc) | [Github](https://github.com/ivantw829)
